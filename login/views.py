ac2f9689d70db6320dfe0cf077af63b7e6a3ed99
Luca Albuquerque <lucalacerda1@gmail.com>
2016-05-17 16:52:17 -0300

    Documentação e algumas alterações

--- login/views.py
+++ login/views.py
@@ -12,6 +12,15 @@ from django.shortcuts import render
 
 @cache.never_cache
 def loginusr(request):
+    """
+    View da página de Login
+
+    Tem duas funções: Processar o reset de senha e fazer o Login.
+    LOGIN: Verifica se o usuário e senhas estão no sistema, caso contrário, informa respectivas mensagens de erro.
+    PASS RESET: Reseta a senha de acordo com o usuário informado, enviando por email a nova senha gerada randomicamente.
+    @param request:
+    @return:
+    """
     template = loader.get_template('login/login.html')
 
     context = {}
@@ -63,41 +72,69 @@ def loginusr(request):
 
 
 def pass_generator(size=8, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(size))
+    """
+    Gera senha para usuários no sistema.
+
+    @param size: Tamanho da senha. Padrão: 8 Caracteres
+    @param chars: Métodos de string para gerar a senha. Padrão: Letras maíusculas e dígitos.
+    @return: Senha gerada de acordo com os parametros passados ou pré definidos.
+    """
+    return ''.join(random.SystemRandom().choice(chars) for _ in range(size))
 
 
 def mudasenha(request):
+    """
+    View para mudar a senha do usuário logado.
 
+    Verifica se o formulário é valido, então prossegue verificando se
+    a nova senha e nova senha confirma são iguais, verifica se a senha antiga é a mesma no sistema. Se não
+    passar em alguma verificação, então retorna seus respectivos erros em state em forma de mensagem para usuário.
+
+    @param request:
+    @return:
+    """
+    form = MudarSenhaForm(request.POST or None)
     context = {}
+    state=''
     context['page'] = "Muda Senha"
     if request.POST:
-        form = MudarSenhaForm(request.POST)
         senha_atual = request.POST['senha_atual']
         nova_senha = request.POST['nova_senha']
         nova_senha_confirma = request.POST['nova_senha_confirma']
-        if nova_senha == nova_senha_confirma:
-            user_auth = User.objects.get(username__exact=request.user.username)
-            if user_auth.check_password(senha_atual):
-                user_auth.set_password(nova_senha)
-                user_auth.save()
-                state = {'msg': "A sua senha foi alterada com sucesso!", 'mode': 'sucess'}
+        if form.is_valid():
+            if nova_senha == nova_senha_confirma:
+                user_auth = User.objects.get(username__exact=request.user.username)
+                if user_auth.check_password(senha_atual):
+                    user_auth.set_password(nova_senha)
+                    user_auth.save()
+                    state = {'msg': "A sua senha foi alterada com sucesso!", 'mode': 'sucess'}
+                else:
+                    state = {'msg': "A Senha Atual digitada não confere com a do sistema.", 'mode': 'error'}
             else:
-                state = {'msg': "A Senha Atual digitada não confere com a do sistema.", 'mode': 'error'}
-        else:
-            state = {'msg': "A confirmação de senha não confere.", 'mode': 'warning'}
+                state = {'msg': "A confirmação de senha não confere.", 'mode': 'warning'}
 
         context['state'] = state
-    else:
-        form = MudarSenhaForm()
 
     context['form'] = form
     return render(request, 'login/mudasenha.html', context)
 
 def webhome(request):
+    """
+    View que renderiza conteúdo a ser mostrado em todas as páginas do home.
+
+    @param request:
+    @return:
+    """
 
     context = {'username' : request.user.username }
     return render(request, 'login/webhome.html', context)
 
 def home(request):
+    """
+    View para fazer logout do sistema
+
+    @param request:
+    @return:
+    """
     logout(request)
     return render(request, 'login/home.html')
